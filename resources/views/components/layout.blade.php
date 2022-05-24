e270239fd2edffea4766db0cb65d646fd46d7ae4
Arfian Joko <github.arfianjo@gmail.com>
2022-05-24 13:21:35 +0700

    first commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,13 +8,28 @@
     <link type="text/css" href="{{ asset('vendors/bootstrap/css/bootstrap.css') }}" rel="stylesheet" />
     <link type="text/css" href="{{ asset('vendors/fontawesome/css/all.css') }}" rel="stylesheet" />
     <link type="text/css" href="{{ asset('css/style.css') }}" rel="stylesheet" />
+    <link type="text/css" href="{{ asset('vendors/owlcarousel2/assets/owl.carousel.css') }}" rel="stylesheet" />
     <script type="text/javascript" src="{{ asset('vendors/jquery/jquery-3.6.0.min.js') }}"></script>
 </head>
-<body class="bg-success">
+<body>
     <x-navbar></x-navbar>
-
-    
+    {{ $slot }}
     <script type="text/javascript" src="{{ asset('vendors/bootstrap/js/bootstrap.js') }}"></script>
     <script type="text/javascript" src="{{ asset('vendors/fontawesome/js/all.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('vendors/owlcarousel2/owl.carousel.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+                navigation : false,
+                slideSpeed : 300,
+                paginationSpeed : 400,
+                items : 1, 
+                itemsDesktop : false,
+                itemsDesktopSmall : true,
+                itemsTablet: true,
+                itemsMobile : true
+            });
+        });
+    </script>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,96 @@
 <x-layout title="Rumah Sakit Umi Barokah">
+    <div class="container-fluid" id="carousel-wrap">
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <img src="{{ asset('images/default.jpg') }}">
+            </div>
+            <div class="item">
+                <img src="{{ asset('images/default.jpg') }}">
+            </div>
+            <div class="item">
+                <img src="{{ asset('images/default.jpg') }}">
+            </div>
+        </div>
+    </div>
+    <section class="bg-light">
+        <div class="container" id="medical-service-wrap">
+            <div class="m-2" >
+                <div class="text-center">
+                    <p class="section-title">Layanan Medis</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="" class="medical-service-content">
+                        <div class="card">
+                            <img src="{{ asset('images/default-thumbnail.jpg') }}" class="card-img-top" alt="...">
+                            <div class="card-body text-center">
+                                <h3 class="fw-300 text-dark">Ub.Maternity</h3>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="" class="medical-service-content">
+                        <div class="card">
+                            <img src="{{ asset('images/default-thumbnail.jpg') }}" class="card-img-top" alt="...">
+                            <div class="card-body text-center">
+                                <h3 class="fw-300 text-dark">Rawat Jalan</h3>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="" class="medical-service-content">
+                        <div class="card">
+                            <img src="{{ asset('images/default-thumbnail.jpg') }}" class="card-img-top" alt="...">
+                            <div class="card-body text-center">
+                                <h3 class="fw-300 text-dark">Rawat Inap</h3>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    <div class="container p-4" class="article-wrap">
+        <div class="m-2">
+            <div class="text-center">
+                <p class="section-title">Artikel Kesehatan</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <a href="">
+                    <div class="card">
+                        <img src="{{ asset('images/default-thumbnail.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Cara menjaga pola makan disaat inggin diet ketat</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="">
+                    <div class="card">
+                        <img src="{{ asset('images/default-thumbnail.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Cara menjaga pola makan disaat inggin diet ketat</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="">
+                    <div class="card">
+                        <img src="{{ asset('images/default-thumbnail.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Cara menjaga pola makan disaat inggin diet ketat</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
 </x-layout>
