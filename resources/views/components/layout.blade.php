7e2b423548457517335aab3eaad6fe4c8c1212e1
Arfian Joko <github.arfianjo@gmail.com>
2022-06-07 08:04:05 +0700

    add carousel

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,18 +9,26 @@
     <link type="text/css" href="{{ asset('vendors/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
     <link type="text/css" href="{{ asset('vendors/tiny-slider/tiny-slider.css') }}" rel="stylesheet">
     <link type="text/css" href="{{ asset('vendors/fontawesome/css/all.css') }}" rel="stylesheet">
+    <link type="text/css" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" rel="stylesheet">
     <link type="text/css" href="{{ asset('css/style.css') }}" rel="stylesheet" id="theme-opt">
     <link type="text/css" href="{{ asset('css/color-skin/skyblue.css') }}" rel="stylesheet" id="color-opt">
-    
+    <script type="text/javascript" src="{{ asset('vendors/jquery/jquery-3.6.0.min.js') }}"></script>
 </head>
 <body class="bg-dark">
     <x-navbar></x-navbar>
     {{ $slot }}
-    <script type="text/javascript" src="{{ asset('vendors/jquery/jquery-3.6.0.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('vendors/bootstrap/bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('vendors/tiny-slider/tiny-slider.js') }} "></script>
+    <script type="text/javascript" src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
     <script type="text/javascript" src="{{ asset('vendors/fontawesome/js/all.js') }} "></script>
     <script type="text/javascript" src="{{ asset('js/plugins.init.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script>
+    var swiper = new Swiper(".mySwiper", {
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+        },
+    });
+</script>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,18 @@
 <x-layout>
-    
+    <section class="bg-half d-table w-100">
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="{{ asset('images/default.jpg') }}">
+                </div>
+                <div class="swiper-slide">
+                    <img src="{{ asset('images/default.jpg') }}">
+                </div>
+                <div class="swiper-slide">
+                    <img src="{{ asset('images/default.jpg') }}">
+                </div>
+            </div>
+        </div>
+        <div class="swiper-scrollbar"></div>
+    </section>
 </x-layout>
